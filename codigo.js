7396a80e69fc35e389d039296ec1801753bfbcd3
William Lima <73370617+willwlima@users.noreply.github.com>
2021-02-23 18:03:04 -0300

    Alterações
    Troca de pin, exclusão de códigos não utilizados

--- codigo.js
+++ codigo.js
@@ -39,49 +39,17 @@ function onMapClick(e) {
         .openOn(map);
 }
 
-// Modelo padrão
-// L.marker([-20.8177769, -49.4011877]).addTo(map)
-//     .bindPopup("<b>Studio WiDi</b><br/>Clique aqui<br> <a href='https://whats.link/sitedesconto'>Agende seu horario</a>"); // .openPopup()
-
-// L.circle([-20.82851, -49.40426], 50, {
-//     color: '#00ff00',
-//     fillColor: ' #0f0',
-//     fillOpacity: 0.5
-// }).addTo(map).bindPopup("I am a circle.");
-
-
-
-// L.polygon([
-// 	[51.509, -0.08],
-// 	[51.503, -0.06],
-// 	[51.51, -0.047]
-// ]).addTo(mymap).bindPopup("I am a polygon.");
-
-// Fim Modelo padrão
-
-// Dom Mercato
-// L.marker([-20.8193961, -49.4043044]).addTo(map)
-//     .bindPopup("<b>Dom Mercato</b><br/>Clique aqui<br> <a href='https://whats.link/sitedesconto'>Agende seu horario</a>"); // .openPopup()
-
-// L.circle([51.508, -0.11], 500, {
-// 	color: 'red',
-// 	fillColor: '#f03',
-// 	fillOpacity: 0.5
-// }).addTo(mymap).bindPopup("I am a circle.");
-
-// L.polygon([
-// 	[51.509, -0.08],
-// 	[51.503, -0.06],
-// 	[51.51, -0.047]
-// ]).addTo(mymap).bindPopup("I am a polygon.");
-
-// Fim
-
-// HOSPITAL DE BASE
+// MODELO
 // L.marker([-20.82848535, -49.39741721]).addTo(map)
 //     .bindPopup("<b>Studio WiDi!</b><br/>Clique aqui<br> <a href='https://whats.link/sitedesconto'>Agende seu horario</a>"); // .openPopup()
 
-// L.circle([-20.82851, -49.40426], 50, {
+// L.circle([-20.82150, -49.40519], 50, {
+//     color: 'yellow',
+//     fillColor: '#ff0',
+//     fillOpacity: 0.5
+// }).addTo(map).bindPopup("I am a circle.");
+
+// L.circle([-20.81733, -49.38599], 50, {
 //     color: 'yellow',
 //     fillColor: '#ff0',
 //     fillOpacity: 0.5
@@ -92,33 +60,37 @@ function onMapClick(e) {
 // 	[51.503, -0.06],
 // 	[51.51, -0.047]
 // ]).addTo(mymap).bindPopup("I am a polygon.");
-
 // Fim
+
 
 // CONFIGURAÇÃO PADRAO ICONES PERSONALIZADOS
 var LeafIcon = L.Icon.extend({
     options: {
         // shadowUrl: 'leaf-shadow.png',
-        iconSize:     [38, 45],
+        iconSize:     [48, 55],
         // shadowSize:   [50, 64],
         iconAnchor:   [28, 25],
         shadowAnchor: [4, 62],
         popupAnchor:  [-8, -10]
     }
 });
-
-
 // DEFINIR MARCADOR PERSONALIZADO
-var lupaIcon = new LeafIcon({iconUrl: '/img/lupa.png'}),
-    secadorIcon = new LeafIcon({iconUrl: '/img/secador.png'});
-    lancheIcon = new LeafIcon({iconUrl: '/img/burguer.png'});
-    pizzaIcon = new LeafIcon({iconUrl: '/img/pizza.png'});
-    farmaciaIcon = new LeafIcon({iconUrl: '/img/farmacia.png'});
+var lupaIcon = new LeafIcon({iconUrl: '/img/pin!.png'}),
+    secadorIcon = new LeafIcon({iconUrl: '/img/pinBeleza.png'});
+    lancheIcon = new LeafIcon({iconUrl: '/img/pinBurguer.png'});
+    pizzaIcon = new LeafIcon({iconUrl: '/img/pinPizza.png'});
+    farmaciaIcon = new LeafIcon({iconUrl: '/img/pinFarmacia.png'});
 
-L.marker([-20.81957, -49.40460], {icon: lupaIcon}).bindPopup("Teste 1.").addTo(map);
+
+// MARCADORES
+L.marker([-20.81957, -49.40460], {icon: lupaIcon}).bindPopup("Em breve...").addTo(map);
+// Studio WiDi
 L.marker([-20.82150, -49.40519], {icon: secadorIcon}).bindPopup("<b>Studio WiDi</b><br/>Clique aqui<br> <a href='tel:173237-6117'>Ligue agora</a><br/> <a href='https://whats.link/studiowidi'>WhatsApp</a><br>Cupom: GRUDINHO").addTo(map);
+// Ticos Burguer
 L.marker([-20.81733, -49.38599], {icon: lancheIcon}).bindPopup("<b>Tico's Burguer</b><br/>Clique aqui<br> <a href='tel:173234-5372'>Ligue agora</a><br>Cupom: GRUDINHO").addTo(map);
+// Jucelinos Pizza
 L.marker([-20.82851, -49.40426], {icon: pizzaIcon}).bindPopup("<b>Jucelino's Pizza</b><br/>Clique aqui<br> <a href='tel:1799221-2980'>Ligue agora</a><br/> <a href='https://whats.link/jucelinospizza'>WhatsApp</a><br>Cupom: GRUDINHO").addTo(map);
+// Rede Central Farmácia
 L.marker([-20.79816, -49.41058], {icon: farmaciaIcon}).bindPopup("<b>Rede Central Farmácia</b><br/>Clique aqui<br> <a href='tel:1732376117'>Ligue agora</a><br/> <a href='https://whats.link/redecentral'>WhatsApp</a><br>Cupom: GRUDINHO").addTo(map);
 
 
